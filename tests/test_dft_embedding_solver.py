59d74c74f607c22837ef4616520e4cc9e332bf2f
Max Rossmannek <oss@zurich.ibm.com>
2023-05-25 18:00:17 +0200

    Add unittests for range-separation limits

--- tests/test_dft_embedding_solver.py
+++ tests/test_dft_embedding_solver.py
@@ -39,6 +39,57 @@ class TestDFTEmbeddingSolver(unittest.TestCase):
 
         self.assertAlmostEqual(result.total_energies[0], -75.930449640258, places=6)
 
+    def test_hf_limit(self):
+        """TODO."""
+        omega = 10000.0
+
+        driver = PySCFDriver(
+            atom="O 0.0 0.0 0.115; H 0.0 0.754 -0.459; H 0.0 -0.754 -0.459",
+            basis="6-31g*",
+            method=MethodType.RKS,
+            xc_functional=f"ldaerf + lr_hf({omega})",
+            xcf_library="xcfun",
+        )
+
+        active_space = ActiveSpaceTransformer(4, 4)
+
+        converter = QubitConverter(ParityMapper(), two_qubit_reduction=True)
+        solver = NumPyMinimumEigensolverFactory(use_default_filter_criterion=True)
+        algo = GroundStateEigensolver(converter, solver)
+
+        dft_solver = DFTEmbeddingSolver(active_space, algo)
+
+        result = dft_solver.solve(driver, omega)
+
+        ref_problem = ActiveSpaceTransformer(4, 4).transform(driver.run())
+        ref_result = algo.solve(ref_problem)
+
+        self.assertAlmostEqual(result.total_energies[0], ref_result.total_energies[0], places=5)
+
+    def test_dft_limit(self):
+        """TODO."""
+        omega = 0.01
+
+        driver = PySCFDriver(
+            atom="O 0.0 0.0 0.115; H 0.0 0.754 -0.459; H 0.0 -0.754 -0.459",
+            basis="6-31g*",
+            method=MethodType.RKS,
+            xc_functional=f"ldaerf + lr_hf({omega})",
+            xcf_library="xcfun",
+        )
+
+        active_space = ActiveSpaceTransformer(4, 4)
+
+        converter = QubitConverter(ParityMapper(), two_qubit_reduction=True)
+        solver = NumPyMinimumEigensolverFactory(use_default_filter_criterion=True)
+        algo = GroundStateEigensolver(converter, solver)
+
+        dft_solver = DFTEmbeddingSolver(active_space, algo)
+
+        result = dft_solver.solve(driver, omega)
+
+        self.assertAlmostEqual(result.total_energies[0], result.hartree_fock_energy, places=5)
+
 
 if __name__ == "__main__":
     unittest.main()
